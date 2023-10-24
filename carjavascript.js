1f5bf6871eb6dbeda6a701ae64df2638c533b2ce
Prince Kumar Ram <132006575+princekumarram@users.noreply.github.com>
2023-10-24 23:39:06 +0530

    Add files via upload

---
+++ carjavascript.js
@@ -0,0 +1,16 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'movingsound.mp3')
+audio.loop=true;
+audio.play();
+
+// let beat=new Audio('sound.mp3');
+// beat.play();
+// beat.load();
+
+
+// var x=document.getElementById("sound.mp3");
+
+// function playAudio(){
+//     x.play();
+
+// }
